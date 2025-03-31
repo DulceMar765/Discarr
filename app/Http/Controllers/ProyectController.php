e231a0b525db82b9953a5ee85254f647b3c6243c
iRivalAether <bryandiaz1810@gmail.com>
2025-03-31 10:40:41 -0600

    1.2 creacion_Model,controladores y migraciones

---
+++ app/Http/Controllers/ProyectController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\proyect;
+use Illuminate\Http\Request;
+
+class ProyectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(proyect $proyect)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(proyect $proyect)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, proyect $proyect)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(proyect $proyect)
+    {
+        //
+    }
+}
