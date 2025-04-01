8aea9249b3cccce24f4086f169c2c37f1a30a3b5
iRivalAether <bryandiaz1810@gmail.com>
2025-03-31 18:24:44 -0600

    1.3 Resolucion de comits

--- app/Models/employee.php
+++ app/Models/employee.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class employee extends Model
+class Employee extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'position',
+        'salary',
+        'hire_date',
+        'address',
+        'status'
+    ];
+
+    protected $casts = [
+        'hire_date' => 'date',
+        'salary' => 'decimal:2',
+        'status' => 'boolean'
+    ];
 }
